7b075b1efb37ffdac71353b55152dbc227f53671
wilicarol <wili.carol0235@gmail.com>
2025-06-25 13:41:10 -0300

    refactor: Organizando plotagem

--- fuzzy_controller.py
+++ fuzzy_controller.py
@@ -3,62 +3,37 @@ import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 import skfuzzy.control as ctrl
 
-
+# Definição dos universos de discurso
 erro_range = np.arange(0, 28.1, 0.1)
 deltaerro_range = np.arange(-1, 1.01, 0.01)
 potencia_range = np.arange(0, 1.01, 0.01)
 
-# Variáveis 
+# Variáveis fuzzy
 Erro = ctrl.Antecedent(erro_range, 'Erro')
 DeltaErro = ctrl.Antecedent(deltaerro_range, 'DeltaErro')
 PotenciaMotor = ctrl.Consequent(potencia_range, 'PotenciaMotor')
 
-# Unidades 
+# Unidades e labels
 Erro.unit = 'm'; Erro.membershipA = 'E'
 DeltaErro.unit = 'm'; DeltaErro.membershipA = 'ΔE'
 PotenciaMotor.unit = '%'; PotenciaMotor.membershipA = 'PM'
 
-# Funções de pertinência
+# Funções de pertinência - Erro
 Erro['P'] = fuzz.trapmf(Erro.universe, [0, 0, 4, 8])
 Erro['M'] = fuzz.trimf(Erro.universe, [6, 14, 20])
 Erro['G'] = fuzz.trapmf(Erro.universe, [18, 22, 28, 28])
 
+# DeltaErro
 DeltaErro['N'] = fuzz.trapmf(DeltaErro.universe, [-1, -1, -0.3, -0.05])
 DeltaErro['ZE'] = fuzz.trimf(DeltaErro.universe, [-0.1, 0.0, 0.1])
 DeltaErro['P'] = fuzz.trapmf(DeltaErro.universe, [0.05, 0.3, 1.0, 1.0])
 
+# PotênciaMotor
 PotenciaMotor['B'] = fuzz.trimf(PotenciaMotor.universe, [0.0, 0.15, 0.3])
 PotenciaMotor['M'] = fuzz.trimf(PotenciaMotor.universe, [0.25, 0.5, 0.75])
 PotenciaMotor['A'] = fuzz.trimf(PotenciaMotor.universe, [0.6, 0.85, 1.0])
 
-# plotagem das funções de pertinência
-fig, axs = plt.subplots(3, 1, figsize=(8, 8))
-
-# Erro
-for label in Erro.terms:
-    axs[0].plot(Erro.universe, Erro[label].mf, label=label)
-axs[0].set_title("Erro [m]")
-axs[0].set_ylabel("Pertinência")
-axs[0].legend(loc="upper right")
-
-# DeltaErro
-for label in DeltaErro.terms:
-    axs[1].plot(DeltaErro.universe, DeltaErro[label].mf, label=label)
-axs[1].set_title("DeltaErro [m]")
-axs[1].set_ylabel("Pertinência")
-axs[1].legend(loc="upper right")
-
-# Potência do Motor
-for label in PotenciaMotor.terms:
-    axs[2].plot(PotenciaMotor.universe, PotenciaMotor[label].mf, label=label)
-axs[2].set_title("Potência do Motor [%]")
-axs[2].set_ylabel("Pertinência")
-axs[2].set_xlabel("Valor")
-axs[2].legend(loc="upper right")
-
-plt.tight_layout()
-plt.show()
-
+# Regras fuzzy baseadas na tabela fornecida
 regras = [
     ctrl.Rule(Erro['P'] & DeltaErro['N'], PotenciaMotor['M']),
     ctrl.Rule(Erro['P'] & DeltaErro['ZE'], PotenciaMotor['B']),
@@ -71,6 +46,33 @@ regras = [
     ctrl.Rule(Erro['G'] & DeltaErro['P'], PotenciaMotor['M']),
 ]
 
+# Função de criação do controlador
 def novo_controlador_fuzzy():
     sistema = ctrl.ControlSystem(regras)
     return ctrl.ControlSystemSimulation(sistema)
+
+# Plotagem
+if __name__ == "__main__":
+    fig, axs = plt.subplots(3, 1, figsize=(8, 8))
+
+    for label in Erro.terms:
+        axs[0].plot(Erro.universe, Erro[label].mf, label=label)
+    axs[0].set_title("Erro [m]")
+    axs[0].set_ylabel("Pertinência")
+    axs[0].legend(loc="upper right")
+
+    for label in DeltaErro.terms:
+        axs[1].plot(DeltaErro.universe, DeltaErro[label].mf, label=label)
+    axs[1].set_title("DeltaErro [m]")
+    axs[1].set_ylabel("Pertinência")
+    axs[1].legend(loc="upper right")
+
+    for label in PotenciaMotor.terms:
+        axs[2].plot(PotenciaMotor.universe, PotenciaMotor[label].mf, label=label)
+    axs[2].set_title("Potência do Motor [%]")
+    axs[2].set_ylabel("Pertinência")
+    axs[2].set_xlabel("Valor")
+    axs[2].legend(loc="upper right")
+
+    plt.tight_layout()
+    plt.show()
